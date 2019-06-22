d17255f34017a99aa57a3fa43b3c56036ea2ec23
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2019-06-22 19:42:37 -0300

    Handlebars template

--- src/app/controllers/PurchaseController.js
+++ src/app/controllers/PurchaseController.js
@@ -1,6 +1,6 @@
 const Ad = require('../models/Ad')
 const User = require('../models/User')
-const Main = require('../services/Mail')
+const Mail = require('../services/Mail')
 
 class PurchaseController {
   async store (req, res) {
@@ -9,11 +9,12 @@ class PurchaseController {
     const purchaseAd = await Ad.findById(ad).populate('author')
     const user = await User.findById(req.userId)
 
-    await Main.sendMail({
+    await Mail.sendMail({
       from: '"Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>"',
       to: purchaseAd.author.email,
       subject: `Solicitação de compra: ${purchaseAd.title}`,
-      html: `<p>Teste: ${content}</p>`
+      template: 'purchase',
+      context: { user, content, ad: purchaseAd }
     })
 
     return res.send()
